40d03d87b1b22fad4bc341303654394dda73c8bd
Shiqi-Meng <2579553820@qq.com>
2024-04-07 18:36:21 +0800

    netty rpc client

---
+++ rpc-simple/src/main/java/org/msq/rpc/rpcsimple/remoting/transport/netty/client/NettyRpcClient.java
@@ -0,0 +1,120 @@
+package org.msq.rpc.rpcsimple.remoting.transport.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.msq.rpc.rpccommon.enums.CompressTypeEnum;
+import org.msq.rpc.rpccommon.enums.SerializationTypeEnum;
+import org.msq.rpc.rpccommon.enums.ServiceDiscoveryEnum;
+import org.msq.rpc.rpccommon.extension.ExtensionLoader;
+import org.msq.rpc.rpccommon.fatory.SingletonFactory;
+import org.msq.rpc.rpcsimple.registry.ServiceDiscovery;
+import org.msq.rpc.rpcsimple.remoting.constants.RpcConstants;
+import org.msq.rpc.rpcsimple.remoting.dto.RpcMessage;
+import org.msq.rpc.rpcsimple.remoting.dto.RpcRequest;
+import org.msq.rpc.rpcsimple.remoting.dto.RpcResponse;
+import org.msq.rpc.rpcsimple.remoting.transport.RpcRequestTransport;
+import org.msq.rpc.rpcsimple.remoting.transport.netty.codec.RpcMessageDecoder;
+import org.msq.rpc.rpcsimple.remoting.transport.netty.codec.RpcMessageEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * initialize and close bootstrap object
+ */
+@Slf4j
+public class NettyRpcClient implements RpcRequestTransport {
+
+    private final ServiceDiscovery serviceDiscovery;
+    private final UnprocessedRequests unprocessedRequests;
+    private final ChannelProvider channelProvider;
+    private final Bootstrap bootstrap;
+    private final EventLoopGroup eventLoopGroup;
+
+    public NettyRpcClient() {
+        eventLoopGroup = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        p.addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
+                        p.addLast(new RpcMessageEncoder());
+                        p.addLast(new RpcMessageDecoder());
+                        p.addLast(new NettyRpcClientHandler());
+                    }
+                });
+        this.serviceDiscovery = ExtensionLoader.getExtensionLoader(ServiceDiscovery.class).getExtension(ServiceDiscoveryEnum.ZK.getName());
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+        this.channelProvider = SingletonFactory.getInstance(ChannelProvider.class);
+    }
+
+    @SneakyThrows
+    public Channel doConnect(InetSocketAddress inetSocketAddress) {
+        CompletableFuture<Channel> completableFuture = new CompletableFuture<>();
+        bootstrap.connect(inetSocketAddress).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                log.info("The client has connected [{}] successful!", inetSocketAddress.toString());
+                completableFuture.complete(future.channel());
+            } else {
+                throw new IllegalStateException();
+            }
+        });
+        return completableFuture.get();
+    }
+
+    @Override
+    public Object sendRpcRequest(RpcRequest rpcRequest) {
+        CompletableFuture<RpcResponse<Object>> resultFuture = new CompletableFuture<>();
+        InetSocketAddress inetSocketAddress = serviceDiscovery.lookupService(rpcRequest);
+        Channel channel = getChannel(inetSocketAddress);
+        if (channel.isActive()) {
+            unprocessedRequests.put(rpcRequest.getRequestId(), resultFuture);
+            RpcMessage rpcMessage = RpcMessage.builder()
+                    .data(rpcRequest)
+                    .codec(SerializationTypeEnum.PROTOSTUFF.getCode())
+                    .compress(CompressTypeEnum.GZIP.getCode())
+                    .messageType(RpcConstants.REQUEST_TYPE)
+                    .requestId(Integer.parseInt(rpcRequest.getRequestId()))
+                    .build();
+            channel.writeAndFlush(rpcMessage).addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    log.info("client send message: [{}]", rpcMessage);
+                } else {
+                    future.channel().close();
+                    resultFuture.completeExceptionally(future.cause());
+                    log.error("send failed:", future.cause());
+                }
+            });
+        } else {
+            throw new IllegalStateException();
+        }
+        return resultFuture;
+    }
+
+    public Channel getChannel(InetSocketAddress inetSocketAddress) {
+        Channel channel = channelProvider.get(inetSocketAddress);
+        if (channel == null) {
+            channel = doConnect(inetSocketAddress);
+            channelProvider.set(inetSocketAddress, channel);
+        }
+        return channel;
+    }
+
+    public void close() {
+        eventLoopGroup.shutdownGracefully();
+    }
+}
